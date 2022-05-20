25debdd25352340b327cfff5394554e3abec1a07
Aron Andrejev <aron.andrejev@gmail.com>
2022-05-20 21:29:11 +0300

    Sqlite3Wrapper as context manager

--- sqlite3wrapper/sqlite3wrapper.py
+++ sqlite3wrapper/sqlite3wrapper.py
@@ -1,13 +1,24 @@
 import sqlite3
 from typing import Iterable
+import logging as log
 
 
 class Sqlite3Wrapper:
-    __slots__ = "__db_path"
+    __slots__ = (
+        "__db_path",
+        "__connection"
+    )
 
     def __init__(self, filepath: str):
         self.__db_path: str = filepath
     
-    def query(self, query: str, values: Iterable = []):
-        with sqlite3.connect(self.__db_path) as con:
-            con.execute(query, values)
+    def __enter__(self) -> sqlite3.Connection:
+        log.debug("Opening connection")
+        self.__connection = sqlite3.connect(self.__db_path)
+        return self.__connection
+
+    def __exit__(self, type, value, traceback):
+        log.debug("Closing connection")
+        log.debug(f"__exit__ arguments: {type}, {value}, {traceback}")
+        self.__connection.commit()
+        self.__connection.close()
