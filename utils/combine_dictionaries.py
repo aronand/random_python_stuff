5e94edae1ebc5d302f4ea6019b335e2a84e5f22e
Aron Andrejev <aron.andrejev@gmail.com>
2022-05-21 11:10:42 +0300

    defaultdict support to combine_dictionaries

--- utils/combine_dictionaries.py
+++ utils/combine_dictionaries.py
@@ -1,12 +1,18 @@
 from collections import defaultdict
+from typing import Callable
 
 
 def combine_dictionaries(*args) -> defaultdict[list]:
-    """Combines n amount of dictionaries into one, which holds all the values in a list."""
+    """Combines n amount of dictionaries into one, which holds all the values in a list.
+    
+     * Supports dicts and defaultdicts
+    """
     new_dict = defaultdict(list)
 
+    dict_types: list[Callable] = [dict, defaultdict]
+
     for dictionary in args:
-        if type(dictionary) is not dict:
+        if type(dictionary) not in dict_types:
             raise TypeError
         
         for key, value in dictionary.items():
